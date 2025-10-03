76a8428f83f24e2a83362bd1fa49c3b94922630f
Mohammad <umar.mohammad.iliyas@swedbank.se>
2025-10-03 11:49:32 +0200

    adds verification code screen

---
+++ app/src/main/java/com/sample/foodhub/ui/features/auth/VerificationScreen.kt
@@ -0,0 +1,117 @@
+package com.sample.foodhub.ui.features.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sample.foodhub.R
+
+@Composable
+fun VerificationScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.ic_signup_bg),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(24.dp, top = 180.dp, bottom = 24.dp, end = 24.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = stringResource(R.string.verification_header),
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = Color.Black,
+                fontSize = 42.sp,
+                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold
+            )
+
+            Text(
+                text = stringResource(R.string.verification_data_detail), modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp),
+                color = Color.Gray,
+                fontSize = 16.sp
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .weight(1f),
+                    singleLine = true,
+                    shape = RoundedCornerShape(8.dp)
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .weight(1f),
+                    singleLine = true,
+                    shape = RoundedCornerShape(8.dp)
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .weight(1f),
+                    singleLine = true,
+                    shape = RoundedCornerShape(8.dp)
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .weight(1f),
+                    singleLine = true,
+                    shape = RoundedCornerShape(8.dp)
+                )
+            }
+
+            Text(
+                text = stringResource(R.string.please_resend_code),
+                modifier = Modifier
+                    .padding(top = 32.dp),
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun VerificationScreenPreview() {
+    VerificationScreen()
+}
